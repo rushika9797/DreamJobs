a8a3239fd6b905460b40852514edff8e4ad6c97d
Rushika Virani <24ceuog166@ddu.ac.in>
2026-04-19 12:56:13 +0530

    modify admin panel

--- MyCareer/portal/admin.py
+++ MyCareer/portal/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile, Job, Application
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Job)
+admin.site.register(Application)
